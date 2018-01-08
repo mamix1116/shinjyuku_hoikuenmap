92fd183c7dc1a510eeb67ac8ed2d95a36e57d467
mamix1116 <mamix1116@gmail.com>
2018-01-08 17:34:14 +0900

    changed

--- js/ol_style.js
+++ js/ol_style.js
@@ -4,7 +4,7 @@
  */
 var featureStyleList = {
 	'default': { color: 'rgba(153, 153, 153, 1)', img: 'image/018.png'},
-	'認証': { color: '#0362A0', img: 'image/019.png'},
+	'認証': { color: '#3388cc', img: 'image/019.png'},
 	'認可外': { color: '#0362A0', img: 'image/019.png'},
 	'幼稚園': { color: '#FF5C24', img: 'image/029.png'},
 	'認可': { color: '#6EE100', img: 'image/018.png'}
